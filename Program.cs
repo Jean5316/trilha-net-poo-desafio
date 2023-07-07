855e868eecab1027cb098383248f8b41c594ec13
Jean5316 <103619912+Jean5316@users.noreply.github.com>
2023-07-07 03:04:45 -0400

    Desafio Completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("99475787", "LX-3", "030005", 8);
+nokia.InstalarAplicativo("Telegram");
+nokia.ReceberLigacao();
+nokia.Ligar();
+
+
+Console.WriteLine("\nSmartphone Iphone");
+Iphone iphone = new Iphone("99475785", "13", "0305", 8);
+iphone.InstalarAplicativo("Netflix");
+iphone.ReceberLigacao();
+iphone.Ligar();
